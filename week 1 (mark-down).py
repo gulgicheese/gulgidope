5f0638c9b1563edf8ee083a06006bff13cfa9ce8
gulgicheese <60563911+gulgicheese@users.noreply.github.com>
2020-02-04 16:47:12 +0800

    Add files via upload

---
+++ week 1 (mark-down).py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook is created for the IBM capstone course exclusively.
+
+# Througout the series, we have learnt some fundamental knowledge of things such as **machine learning with python**, **data visualization in seaborn**, and other **data science methodology & algorithm**. 
+
+# > As we have just created the *repository* on [github] (http://github.com)
+
+# #### let's do some practice
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
